b4be3050308b96e8384f180a7446afa6719152ed
Austin Zane <austin.zane@berkeley.edu>
2024-02-15 10:26:20 -0800

    Just adding some comments for future functionality.

--- experiments/train.py
+++ experiments/train.py
@@ -6,8 +6,21 @@ from common.datasets import FASTDataset
 
 
 
+def test_all(loader, model, criterion):
+    """
+    Test the model on the entire dataset
+    :param loader:
+    :param model:
+    :param criterion:
+    :return:
+    """
+
+    return 0
+
 
 def main():
+    # wandb.init(project='02_15_2024_ucsf_fast', entity='john-doe')
+
     # Might change this to get the directory from the config file
     data_dir = '/scratch/users/austin.zane/ucsf_fast/data/pilot_labeling/AnnotationData/MorisonPouchMasks_1-23'
 
@@ -17,10 +30,10 @@ def main():
         transforms.Normalize(mean=[0.5], std=[0.5])
     ])
 
-    # Create the dataset and dataloader
-    # Only one dataset for now. Will add test and train later.
+    # Create the datasets and dataloaders
 
     img_dataset = FASTDataset(data_dir, transform=pil2tensor)
+
     n_total = len(img_dataset)
     n_train = int(0.8 * n_total)
     n_test = n_total - n_train
@@ -29,6 +42,11 @@ def main():
 
     loader_train = DataLoader(dataset_train, batch_size=4, shuffle=True, num_workers=4)
     loader_test = DataLoader(dataset_test, batch_size=4, shuffle=True, num_workers=4)
+
+    # model =
+    # criterion =
+    # optimizer =
+    # scheduler =
 
     for img, mask in loader_train:
         print(img.shape, mask.shape)
