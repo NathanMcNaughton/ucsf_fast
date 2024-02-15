062694960febd6fa787613d9b4362920aae3db1f
Austin Zane <austin.zane@berkeley.edu>
2024-02-14 18:01:28 -0800

    I modified the FASTDataset class to account for different image sizes. Still needs to be tested.

--- common/datasets/__init__.py
+++ common/datasets/__init__.py
@@ -1,6 +1,7 @@
 import os
 import torch
 from torch.utils.data import DataLoader, Dataset, TensorDataset
+from PIL import Image
 import numpy as np
 from torchvision.transforms import transforms
 
@@ -32,30 +33,40 @@ class FASTDataset(Dataset):
     The images and the segmentation labels are saved as PNG images in the same 
     directory. There are around 350 such pairs.
     """
-    def __init__(self, data_dir, transform=None):
-        self.directory = directory
+    def __init__(self, data_dir, resize=True, transform=None):
+        self.data_dir = data_dir
         self.transform = transform
+        self.resize = resize
         # List all files in directory and filter out segmentation label images
-        self.image_files = [f for f in os.listdir(directory) if not '_Morison' in f]
+        self.image_files = [f for f in os.listdir(data_dir) if (not '_Morison' in f and not '.db' in f and not '.json' in f)]
         
     def __len__(self):
         return len(self.image_files)
     
     def __getitem__(self, index):
-        img_name = self.image_files[idx]
+        img_name = self.image_files[index]
         
         # Construct a segmentation label name based on the image name
-        label_name = img_name.rsplit('.', 1)[0] + '_Morrison.png'
+        label_name = img_name.rsplit('.', 1)[0] + '_Morison.png'
         
-        img_path = os.path.join(self.directory, img_name)
-        label_path = os.path.join(self.directory, label, name)
+        img_path = os.path.join(self.data_dir, img_name)
+        label_path = os.path.join(self.data_dir, label_name)
         
         image = Image.open(img_path).convert('L')
         label = Image.open(label_path).convert('L')
         
+        if self.resize and image._size != (960, 720):
+            resize_transform = transforms.Resize((720, 960))
+            image = resize_transform(image)
+            label = resize_transform(label)
+        
         if self.transform:
             image = self.transform(image)
             label = self.transform(label)
+        else:
+            tensor_transform = transforms.ToTensor()
+            image = tensor_transform(image)
+            label = tensor_transform(label)
         
         return image, label
     
---
+++ experiments/train.py
@@ -0,0 +1,5 @@
+
+
+
+
+from common.datasets import FASTDataset
