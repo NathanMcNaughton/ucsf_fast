73745962f6c3dcbf9f579e6437b22f5a4a2f0d0a
Austin Zane <austin.zane@berkeley.edu>
2024-02-14 19:55:41 -0800

    Tweaked dataset class to follow best practices. Still have not properly tested.

--- common/datasets/__init__.py
+++ common/datasets/__init__.py
@@ -32,41 +32,49 @@ class FASTDataset(Dataset):
     This dataset is designed to work with the pilot labeling done in February 2024.
     The images and the segmentation labels are saved as PNG images in the same 
     directory. There are around 350 such pairs.
+    
+    This class defaults to resizing the images when they have irregular dimensions.
+    If the option is turned off and this class is used with torch.utils.data.DataLoader,
+    there will likely be errors when PyTorch tries to stack the images in a batch.
+    
+    Attributes:
+        data_dir (str): Directory with image files.
+        resize (bool): Flag to resize images to a standard size if they don't match.
+        transform (transforms.]): Transformations to be applied to the images.
     """
-    def __init__(self, data_dir, resize=True, transform=None):
+    DEFAULT_IMAGE_SIZE = (960, 720)
+    
+    def __init__(self, data_dir: str, resize: bool = True, transform = None):
         self.data_dir = data_dir
-        self.transform = transform
+        self.transform = transform if transform is not None else transforms.ToTensor()
         self.resize = resize
         # List all files in directory and filter out segmentation label images
-        self.image_files = [f for f in os.listdir(data_dir) if (not '_Morison' in f and not '.db' in f and not '.json' in f)]
+        self.image_files = [f for f in os.listdir(data_dir) if not '_Morison' in f and f.endswith('.png')]
         
     def __len__(self):
         return len(self.image_files)
     
     def __getitem__(self, index):
         img_name = self.image_files[index]
-        
-        # Construct a segmentation label name based on the image name
         label_name = img_name.rsplit('.', 1)[0] + '_Morison.png'
         
         img_path = os.path.join(self.data_dir, img_name)
         label_path = os.path.join(self.data_dir, label_name)
         
-        image = Image.open(img_path).convert('L')
-        label = Image.open(label_path).convert('L')
+        try:
+            image = Image.open(img_path).convert('L')
+            label = Image.open(label_path).convert('L')
+        except IOError as e:
+            raise RuntimeError(f'Error opening image: {e}')
         
-        if self.resize and image._size != (960, 720):
-            resize_transform = transforms.Resize((720, 960))
+        if self.resize and image.size != self.DEFAULT_IMAGE_SIZE:
+            resize_transform = transforms.Resize((self.DEFAULT_IMAGE_SIZE[1], 
+                                                  self.DEFAULT_IMAGE_SIZE[0]))
             image = resize_transform(image)
             label = resize_transform(label)
         
-        if self.transform:
-            image = self.transform(image)
-            label = self.transform(label)
-        else:
-            tensor_transform = transforms.ToTensor()
-            image = tensor_transform(image)
-            label = tensor_transform(label)
+        image = self.transform(image)
+        label = self.transform(label)
         
         return image, label
     
